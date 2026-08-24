cabe81c93ddddb268ab113e0a60b444e452caa10
William Louis-Louisy <87181045+William-Louis-Louisy@users.noreply.github.com>
2026-08-24 12:02:19 +0200

    fix: configure a stable intl timezone

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -2,6 +2,7 @@ export const APP_LOCALES = ["fr", "en"] as const;
 export type AppLocale = (typeof APP_LOCALES)[number];
 
 export const DEFAULT_LOCALE: AppLocale = "fr";
+export const APP_TIME_ZONE = "UTC";
 export const LOCALE_COOKIE = "book-wishlist-locale";
 
 export function isAppLocale(value: string | undefined): value is AppLocale {
